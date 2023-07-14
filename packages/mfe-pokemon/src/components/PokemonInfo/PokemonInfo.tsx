4ec31a9a2fa72b2efe5a859d78829f03563402b8
José Manuel Piñeiro García <josempga@ext.inditex.com>
2023-07-14 01:00:52 +0100

    Add gap to pokemon info

--- packages/mfe-pokemon/src/components/PokemonInfo/PokemonInfo.tsx
+++ packages/mfe-pokemon/src/components/PokemonInfo/PokemonInfo.tsx
@@ -7,39 +7,40 @@ interface PokemonInfoProps {
 
 export const PokemonInfo = ({ pokemon }: PokemonInfoProps) => {
   return (
-    <Container padding={['m', 'none']}>
-      <FlexBox direction="column">
-        <FlexBox>
-          <Text transform="capitalize">abilities:</Text>
-          <List direction="row">
-            {pokemon.details?.abilities?.map((ability) => (
-              <Tooltip
-                key={ability.id}
-                trigger="hover"
-                target={
-                  <Tag key={ability.id} variant="primary">
-                    {ability.name}
-                  </Tag>
-                }
-              >
-                <List>
-                  {ability.effects.map((effect) => (
-                    <ListItem key={effect}>{effect}</ListItem>
-                  ))}
-                </List>
-              </Tooltip>
-            ))}
-          </List>
-        </FlexBox>
-        <Container>
-          <Text transform="capitalize">height:</Text>
-          <Tag variant="primary">{pokemon.details?.height}</Tag>
-        </Container>
-        <Container>
-          <Text transform="capitalize">weight:</Text>
-          <Tag variant="primary">{pokemon.details?.weight}</Tag>
-        </Container>
+    <FlexBox direction="column" gap="s" padding={['m', 'none']}>
+      <FlexBox>
+        <Text transform="capitalize">abilities:</Text>
+        <List direction="row">
+          {pokemon.details?.abilities?.map((ability) => (
+            <Tooltip
+              key={ability.id}
+              trigger="hover"
+              width="content"
+              target={
+                <Tag key={ability.id} variant="primary">
+                  {ability.name}
+                </Tag>
+              }
+            >
+              <List>
+                {ability.effects.map((effect) => (
+                  <ListItem key={effect}>
+                    <Text variant="caption">{effect}</Text>
+                  </ListItem>
+                ))}
+              </List>
+            </Tooltip>
+          ))}
+        </List>
       </FlexBox>
-    </Container>
+      <Container>
+        <Text transform="capitalize">height:</Text>
+        <Tag variant="primary">{pokemon.details?.height}</Tag>
+      </Container>
+      <Container>
+        <Text transform="capitalize">weight:</Text>
+        <Tag variant="primary">{pokemon.details?.weight}</Tag>
+      </Container>
+    </FlexBox>
   )
 }
